a385f6ede272680fdbf81b386948da93b3d7d88e
Olexandr Molodchenko <oleksandr@innocode.no>
2016-07-15 12:27:12 +0300

    add backups with DATABASE_URL

--- backup_postgres.rb
+++ backup_postgres.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'net/ssh'
 require 'shellwords'
 require 'pry'
+require 'uri'
 
 action = ARGV[0]
 
@@ -33,35 +34,62 @@ remote_app_name = url.match(/.*:(.*)/)
 @username = "#{remote_name}"
 @hostname = "#{parsed_url[1]}"
 
-@dbhostname = "config:get #{remote_app_name[1]} DATABASE_HOSTNAME"
-@dbname = "config:get #{remote_app_name[1]} DATABASE_NAME"
-@dbpass = "config:get #{remote_app_name[1]} DATABASE_PASSWORD"
-@dbusername = "config:get #{remote_app_name[1]} DATABASE_USERNAME"
+@dbhostname_cmd = "config:get #{remote_app_name[1]} DATABASE_HOSTNAME"
+@dbname_cmd = "config:get #{remote_app_name[1]} DATABASE_NAME"
+@dbpass_cmd = "config:get #{remote_app_name[1]} DATABASE_PASSWORD"
+@dbusername_cmd = "config:get #{remote_app_name[1]} DATABASE_USERNAME"
+@file_name = "db_backup.#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.sqlc"
+@dburl_cmd = "config:get #{remote_app_name[1]} DATABASE_URL"
 
 
 begin
+  ssh = Net::SSH.start(@hostname, @username)
+  url = (ssh.exec!(@dburl_cmd))
+  ssh.close
+  puts url
+end
+
+if url.nil? || url.empty?
+
+
+  url = URI.parse(url.strip)
+    database_hostname = url.host
+    database_name = url.path[1..-1]
+    database_password = url.password.gsub(/\n/,"").shellescape
+    database_username = url.user
+    puts 'database_hostname   ' + database_hostname
+    puts 'database_name   ' + database_name
+    puts 'database_password   ' + database_password
+    puts 'database_username   ' +  database_username
+    puts "tratatat"
+else
+
+begin
     ssh = Net::SSH.start(@hostname, @username)
-    database_hostname = (ssh.exec!(@dbhostname)).gsub(/\n/,"").shellescape
-    database_name = (ssh.exec!(@dbname)).gsub(/\n/,"").shellescape    
-    database_password = (ssh.exec!(@dbpass)).gsub(/\n/,"").shellescape
-    database_username = (ssh.exec!(@dbusername)).gsub(/\n/,"").shellescape
-    file_name = "db_backup.#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.sqlc"
+    database_hostname = (ssh.exec!(@dbhostname_cmd)).gsub(/\n/,"").shellescape
+    database_name = (ssh.exec!(@dbname_cmd)).gsub(/\n/,"").shellescape    
+    database_password = (ssh.exec!(@dbpass_cmd)).gsub(/\n/,"").shellescape
+    database_username = (ssh.exec!(@dbusername_cmd)).gsub(/\n/,"").shellescape
     ssh.close
   rescue
     puts "Unable to connect to #{@hostname} using #{@username}/#{password}"
+end
 end
 
 case action
 #----------------------------------------------------------------------------------------
 when "backup"
-  @mkdir = "mkdir -p #{@backups_path_remote}"
-  @backupdb = "PGPASSWORD=#{database_password} pg_dump -U #{database_username} -h #{database_hostname} #{database_name} -Fc --file=#{@backups_path_remote}/#{file_name}"
+
+  puts 'backuping process'
+  @mkdir_cmd = "mkdir -p #{@backups_path_remote}"
+  @backupdb_cmd = "PGPASSWORD=#{database_password} pg_dump -U #{database_username} -h #{database_hostname} #{database_name} -Fc --file=#{@backups_path_remote}/#{@file_name}"
 
 begin
   ssh = Net::SSH.start(@hostname, @ubuntu_user)
-    backups_path = ssh.exec!(@mkdir)
-    backup = ssh.exec!(@backupdb)
+    backups_path = ssh.exec!(@mkdir_cmd)
+    backup = ssh.exec!(@backupdb_cmd)
     ssh.close
+    puts 'end backup process'
 end
 
 #-------------------------------------------------------------------------------------
@@ -79,20 +107,21 @@ end
 #--------------------------------------------------------------------------------------------
 when "restore"
    
-    @pgpass = "touch ~/.pgpass && chmod 0600 ~/.pgpass"
-    @credentials = "echo '*:*:*:#{database_username}:#{database_password}' > ~/.pgpass"
-    @db_restore = "PGPASSFILE=~/.pgpass pg_restore --no-owner -h #{database_hostname} -p 5432 -d #{database_name} #{file_name_last}"
-    @rmpgpass = "rm ~/.pgpass"
+    @pgpass_cmd = "touch ~/.pgpass && chmod 0600 ~/.pgpass"
+    @credentials_cmd = "echo '*:*:*:#{database_username}:#{database_password}' > ~/.pgpass"
+    @db_restore_cmd = "PGPASSFILE=~/.pgpass pg_restore --no-owner -h #{database_hostname} -p 5432 -d #{database_name} #{file_name_last}"
+    @rmpgpass_cmd = "rm ~/.pgpass"
 begin
   ssh = Net::SSH.start(@hostname, @ubuntu_user)
-    pg_pass = ssh.exec!(@pgpass)
-    db_credentials = ssh.exec!(@credentials)
-    backups_path = ssh.exec!(@last_backup)
+    pg_pass = ssh.exec!(@pgpass_cmd)
+    db_credentials = ssh.exec!(@credentials_cmd)
+    backups_path = ssh.exec!(@last_backup_cmd)
     file_name_last = backups_path.split[-1]
-    restore_db = ssh.exec!(@bd_restore)
-    rmpgpass = ssh.exec!(@rmpgpass)
+    restore_db = ssh.exec!(@bd_restore_cmd)
+    rmpgpass = ssh.exec!(@rmpgpass_cmd)
     ssh.close
 end
+else
+  puts 'Action does'n exist ' + action
+
 end
-
-
